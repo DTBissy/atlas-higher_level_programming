6c170e7a2f4034bde8f339f94464a8b639c1642c
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-01 23:26:33 -0500

    Lessthne

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        print("{}".format(chr(ord(char) - 32) if 96 <= ord(
-        char) <= 123 else char), end="")
-        print()
+        if 97 <= char <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
