f0de2b083deb62c6168924bcb157ab8e4e74a1b6
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-02 12:40:30 -0500

    Lets see

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if char not in(97, 123):
+        if char not in(97, 123) and char not in(60, 95) :
             char = chr(ord(char) - 32)
         print("{}".format(char), end="")
     print("")
