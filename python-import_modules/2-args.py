f80db484b1794e96e45b38767fb535b32648185c
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-02 16:32:00 -0500

    we got her boys

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+if __name__ == "__main__":
+  num = len(argv)
+
+  if num < 2:
+      print("No arguments")
+
+  if num == 2:
+      print("1 argument: ")
+      print("1: {}".format(argv[1]))
+  else:
+      print("{} arguments ".format(num - 1))
+      for i in range(1, num):
+          print("{}: {}".format(i, argv[i]))
