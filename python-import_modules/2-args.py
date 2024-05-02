21194d1b9176023a6ae3f713cbd7a5c6db2dfbd8
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-02 16:36:47 -0500

    is it that?

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
       print("1: {}".format(argv[1]))
   else:
       print("{} arguments:".format(num - 1))
-      for i in range(1, num):
+      for i in range(2, num):
           print("{}: {}".format(i, argv[i]))
