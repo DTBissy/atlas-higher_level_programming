347ebe9b7966729ae60443f77e0f05ab3b09c444
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-02 15:00:25 -0500

    Lets ee now

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def imp():
+from add_0 import add
+if __name__ == "__main__":
   a = 1
   b =2
   result = add(a, b)
+
+  print("{a} + {b} = {result}".format(a=a, b=b, result=add))
