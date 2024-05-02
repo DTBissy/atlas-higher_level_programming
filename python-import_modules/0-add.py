28968bd1469c6099f6a0156cdd78acbfa238cd68
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-02 15:04:25 -0500

    Fixed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-  a = 1
-  b = 2
-  result = add(a, b)
-  print("{a} + {b} = {result}".format(a=a, b=b, result=add(a, b)))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{a} + {b} = {result}".format(a=a, b=b, result=add(a, b)))
