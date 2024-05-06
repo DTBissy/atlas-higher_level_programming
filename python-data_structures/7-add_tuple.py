8e05a0996a7882f00d5b2e8c0b5f92cf4ab291ac
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-06 14:06:10 -0500

    Have you ever

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup = ()
+    if len(tuple_a) < 1:
+        tuple_a = tuple_a +(0, 0)
+    elif len(tuple_a) < 2:
+        tuple_a = tuple_a +(0,)
+    if len(tuple_b) < 1:
+        tuple_b = tuple_b + (0, 0)
+    elif len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+    for i, j in zip(tuple_a, tuple_b):
+        tup = tup + (i + j,)
+    final = (tup[0], tup[1])
+    return (final)
