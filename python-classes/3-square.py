9650a8ee087560d8e4847db8af6efc7cfdee112e
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-10 15:49:57 -0500

    __size

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""For my square"""
+
+
+class Square:
+    """ Initializing Class for my square."""
+    def __init__(self, size=0):
+        """ New sqaure
+
+
+        Args:
+        size (int): Size of square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Area of sqaure"""
+        return(self.__size * self.__size)
