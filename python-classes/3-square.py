fba471caed04aeade26ce59308ffbff0276c06d5
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-10 16:00:42 -0500

    WOah

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """ Area of sqaure"""
-        return (self.__size * self.__size)
