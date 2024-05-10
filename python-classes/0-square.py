2257a6419f8dceaac329d1129291ec234664f1c6
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-10 15:21:20 -0500

    Size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,4 +4,10 @@
 
 class Square:
     """ Initializing Class for my square."""
-    pass
+    def __init__(self, size):
+        """ New sqaure
+
+
+        Args:
+        size (int): Size of square"""
+        self.__size = size
