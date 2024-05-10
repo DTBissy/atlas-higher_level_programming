ff290639820eff3df994ebb2cea86bbeab91de74
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-10 15:56:41 -0500

    __size

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,11 +10,20 @@ class Square:
 
         Args:
         size (int): Size of square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter """
+        return self.__size
+
+    @size.setter
+    def size(size, value):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """ Area of sqaure"""
