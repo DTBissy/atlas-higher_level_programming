541b9bf620a86d60760bedb1e52f4e64158713c4
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-10 15:22:18 -0500

    Whoops

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,10 +4,4 @@
 
 class Square:
     """ Initializing Class for my square."""
-    def __init__(self, size):
-        """ New sqaure
-
-
-        Args:
-        size (int): Size of square"""
-        self.__size = size
+    pass
