403ae434977579542406781ae7fffa6b5ead83ce
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-12 17:03:34 -0500

    Im gonna die

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This a function that returns the sum of two integers
+    """
+def add_integer(a, b=98):
+    """The sum of two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be a integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return a + b
