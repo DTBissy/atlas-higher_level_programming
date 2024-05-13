4b0b2f06e45d493d4b5633c1c4b657f763f426e1
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-13 13:12:43 -0500

    Big money

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,4 +10,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be a integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a + b)
