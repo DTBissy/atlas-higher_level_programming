f707b3d32c58e0a5d38280b893be61c9b4df68a3
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-14 14:05:28 -0500

    Lk

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This is for rectangle """
+
+
+class Rectangle:
+    """ This is a rectangle class"""
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This is for rectangle """
+
+
+class Rectangle:
+    """ This is a rectangle class"""
+    def __init__(self, width=0, height=0):
+        """This method initializes its properties"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise TypeError("width must be >= 0")
+        self.__widthwidth = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """this is my width getter"""
+        return self.__width
+
+    @width.setter
+    def width(width, value):
+        """This is my width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("widht must be >= 0")
+
+    @property
+    def height(self):
+        """this is my height getter"""
+        return self.__height
+
+    @height.setter
+    def height(height, value):
+        """This is my height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
