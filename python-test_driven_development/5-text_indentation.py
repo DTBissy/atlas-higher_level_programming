c0295ab5566950ab3d278b4a27c53900fca6e16c
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-13 20:56:39 -0500

    Gah damn

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,16 +4,16 @@
 
 def text_indentation(text):
     """ this returns a indented text after checking edge cases"""
-    if not(isinstance(text, str)):
+    if type(text) is not str:
         raise TypeError("text must be a string")
     new_line = True
     for char in text:
-        if char == ' ' and new_line:
-            continue
-        elif char in ['.', '?', ':']:
+        if char != ' ' or new_line is False:
+            print(char, end="")
+            new_line = False
+        elif char != ' ':
+            new_line = False
+        if char in ['.', '?', ':']:
             print()
             print()
             new_line = True
-        else:
-            print(char, end="")
-            new_line = False
