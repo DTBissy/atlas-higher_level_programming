da0ca3c1ade9eb14ad05d29d808b0d0c955f86e3
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-14 15:01:22 -0500

    sabmit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -29,6 +29,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -42,6 +43,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """this returns a area"""
--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -23,12 +23,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(width, value):
+    def width(self, value):
         """This is my width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -36,12 +37,13 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(height, value):
+    def height(self, value):
         """This is my height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """this returns a area"""
