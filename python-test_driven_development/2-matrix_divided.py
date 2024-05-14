8a89c3a7d97d5234fcefb025980059f4ad4f5483
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-13 19:12:43 -0500

    Submit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,21 @@
 """This is a function for to divide a matrix"""
 
 def matrix_divided(matrix, div):
+    """This checks if number is a integer or float
+
+    Args:
+        matrix (_type_): The matrix we hold
+        div (_type_): What the matrix is divided for
+
+    Raises:
+        TypeError: For if the list isnt a list with integers or floats
+        TypeError: For if the list length isnt the same length as the first
+        TypeError: For if the value isnt a number or float
+        ZeroDivisionError: For if the number is a zero
+
+    Returns:
+        div
+    """
     if not all(isinstance(row, list) and all(isinstance(element, (float, int)
     ) for element in row) for row in matrix):
         raise TypeError("Matrix must be a matrix (list of list) of integers/floats")
