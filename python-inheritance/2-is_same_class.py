265eabee3fb8e383f61853c6741302c624c6da4e
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-18 16:11:47 -0500

    Did i just speed run that

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""THis is to return if a object is
+exaclty the same as another"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
