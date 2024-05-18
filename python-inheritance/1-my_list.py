9b64f7e8c9063968f85ccfe8336eea57c2900ba8
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-18 15:33:00 -0500

    Pour a pint of wock

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Makes Sum Shake"""
+
+
 class MyList(list):
     def print_sorted(self):
+        """Sort() prints a lists in ascending order"""
         self.sort()
         print(self)
