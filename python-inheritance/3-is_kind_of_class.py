d2b512d0dc46d409ca80f07fcdc5999cb02ce1b9
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-18 16:24:12 -0500

    ooof

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """This returns bool for if a object is inherited"""
 
 
-def is_a_kind_of_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """THis will return my boolksdkldslkfldaskf"""
 
     return isinstance(obj, a_class)
