d7341d6d225f91275ee2f62130608fc3f986f6ec
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-18 16:12:53 -0500

    dsds

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ exaclty the same as another"""
 
 def is_same_class(obj, a_class):
     """THis returns true or false is a_clas is mathced"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
