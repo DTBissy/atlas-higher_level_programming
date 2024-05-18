6f89bff4a071169fad60adaf9cbdb332066ddcf0
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-18 16:43:59 -0500

    ds

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -3,6 +3,7 @@
 
 
 class BaseGeometry:
-    """"""
+    """Class for my SuperGeometryClass"""
     def area(self):
+        """Soon to be aread func"""
         raise Exception("area() is not implemented")
