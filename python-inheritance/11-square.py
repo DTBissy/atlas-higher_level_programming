0dc86eb6f6807eb88b20546274c48764214ebf53
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-19 15:34:52 -0500

    xsa

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This is a square that inherits from a thing"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This will be the square class"""
+
+    def __init__(self, size):
+        """This is for square methods and attributes
+        And super overwrites the original classes __init__ method
+        with our new one for
+        the new class. But behaves the same"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
