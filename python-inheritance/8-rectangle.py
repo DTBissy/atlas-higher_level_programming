2f38a798320999f8f82cbb5b5e6e1b5126a38df9
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-19 15:07:41 -0500

    sa

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Im gonna do it. IM GONNA IMPORT"""
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """This is a rectangle thats inherting
