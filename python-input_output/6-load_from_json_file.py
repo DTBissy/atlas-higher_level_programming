f9845c3f5376d868eec5e65e9392e68c7b31b08d
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-21 17:34:23 -0500

    sa

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """THis creats a object from a JSON file"""
-    with open(filename, mode='r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
