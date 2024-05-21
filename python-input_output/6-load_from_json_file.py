56c12a1dedbcd2850087911ab960d1742fea8486
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-21 17:32:52 -0500

    sasa

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-"""THis"""
+"""This defines a function that creates from JSON fle"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """THis creats a object from a JSON file"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
