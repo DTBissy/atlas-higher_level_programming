b001d25736a848d211b5be2f3fbaccd51b5e52c1
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-23 13:57:10 -0500

    lets bsee

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This will be my base class for almost_a_circle"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects
+            Base.__nb_objects += 1
