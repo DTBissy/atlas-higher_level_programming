1a0d4aed5166d8e7d34cbb0a35b1ec9f56c4ee2e
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-23 15:25:35 -0500

    Fourth fix their allowed to Zero

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,13 +22,13 @@ class Rectangle(Base):
 
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
-        elif x < 0:
+        elif x <= 0:
             raise ValueError("x must be >= 0")
         self.__x = x
 
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
-        elif y < 0:
+        elif y <= 0:
             raise ValueError("y must be >= 0")
         self.__y = y
 
