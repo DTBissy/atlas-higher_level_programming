0e9d4711e89a33c14a094514b046aa8939652858
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-25 12:53:29 -0500

    sa

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,6 +6,8 @@ from models.rectangle import *
 
 
 class Square(Rectangle):
+    """This my Square class which inherits from Rectangle and
+    uses super to initialize the attritbutes from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
