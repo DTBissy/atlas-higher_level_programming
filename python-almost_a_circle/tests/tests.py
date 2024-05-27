04370f74233d133598c57abcba6a74789a17144e
{DTBissy} <{isaactheedwards@gmail.com}>
2024-05-27 13:10:30 -0500

    Whatever

---
+++ python-almost_a_circle/tests/tests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This will hold a mahority if not all of my UnitTest Cases"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_default_id_assignment(self):
+        """Test the instances without an id already assigned and should
+        return a 1"""
+        b1 = Base()
+        b2 = Base()
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+
+    def test_stated_id_assignment(self):
+        """This tests that the instance assignes to what id= is assigned to"""
+        b1 = Base(id=42)
+        b2 = Base(id=5)
+
+        self.assertEqual(b1.id, 42)
+        self.assertEqual(b2.id, 5)
+
+    def test_incremnt(self):
+        """This tests the increment if one id already exists"""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(Base.__nb_objects, 2)
