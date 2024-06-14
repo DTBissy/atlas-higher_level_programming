b26025ea6e2cefde98c16077f6441dd888866529
Dtbissy <isaactheedwards@gmail.com>
2024-06-14 13:41:05 -0500

    Service mysql start

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
 -- Show Database
 SHOW DATABASES;
-Database                                                                                   
-information_schema                                                                         
-mysql                                                                                      
-performance_schema                                                                         
-sys  
