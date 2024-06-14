f318e2a1ad102983b754d43a2827649f45b6091a
Dtbissy <isaactheedwards@gmail.com>
2024-06-14 14:23:50 -0500

    There

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
---Creates Database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-=======
 -- Creates Database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
->>>>>>> 42b48e6 (Needs a space between comment starter and commemt)
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
