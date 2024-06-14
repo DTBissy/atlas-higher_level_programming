5e142ef587ffa92a1d29380f65ea9401f34cc350
Dtbissy <isaactheedwards@gmail.com>
2024-06-14 14:20:35 -0500

    Needs a space between comment starter and commemt

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 --Creates Database
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+=======
+-- Creates Database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+>>>>>>> 42b48e6 (Needs a space between comment starter and commemt)
