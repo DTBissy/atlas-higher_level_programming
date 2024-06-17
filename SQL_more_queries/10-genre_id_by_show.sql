5d5c86294ad50dfdc8c548b4079085020081e787
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 16:19:26 -0500

    Fixed typo

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- Shows all shows with a genre link
 SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id ORDER BY tv.shows.title ASC, tv_shows_genres.show_id ASC;
+JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id ORDER BY tv_shows.title ASC, tv_shows_genres.show_id ASC;
