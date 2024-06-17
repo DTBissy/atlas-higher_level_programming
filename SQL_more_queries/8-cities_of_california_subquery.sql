416d9e93ff4d1fd4c550b39a9313744976e444ba
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 15:20:59 -0500

    It already assumes where in the hbtn_0d database

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- SHows all cities in cali
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
