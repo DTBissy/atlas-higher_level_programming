15a7c763c7c796981340d4447fbb380723d8f1f6
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 15:05:29 -0500

    Now no ''

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates user and database 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS user_02_d@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_02_d@localhost IDENTIFIED BY user_0d_2_pwd;
 
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
