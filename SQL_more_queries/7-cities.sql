ead7625856d305f5df8df5e84b37795046b437c9
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 15:17:22 -0500

    oVER DID IT

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- Creates cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE cities (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    state_id INT NOT NULL FOREIGN KEY(id, states),
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL
     name VARCHAR(256) NOT NULL
 );
