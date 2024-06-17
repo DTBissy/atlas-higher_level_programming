c6526b42b72baee50691d71efb21ce38c864014c
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 15:19:21 -0500

    removed , line 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
