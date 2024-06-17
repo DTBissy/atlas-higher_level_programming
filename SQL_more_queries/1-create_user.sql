e3ebbea963759e89d4895d4ccb4c2c15599d0590
Dtbissy <isaactheedwards@gmail.com>
2024-06-17 15:15:41 -0500

    Ok no spaces

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Makes a user
+-- Creatws user if not already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVELIGES ON *.* TO 'user_0d_1'@'localhost';
