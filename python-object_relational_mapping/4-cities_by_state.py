7e033079d97c125a6b4e7c335ccb40108c2b3a30
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:50:40 -0500

    pycode

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ def list_cities():
     """Works on database"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    
+     
     cur = db.cursor()
     query = "SELECT cities.id, cities.name, states.name FROM cities\
             JOIN states ON cities.state_id = states.id;"
@@ -21,6 +21,6 @@ def list_cities():
     cur.close()
     db.close()
 
+
 if __name__ == "__main__":
     list_cities()
-    
