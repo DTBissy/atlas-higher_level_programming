16d4633ce5cfbb1f8ecb261d162f5d65640c53c4
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 14:51:42 -0500

    Pycode

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,12 +3,13 @@
 import MySQLdb
 import sys
 
+
 def print_state_n():
     """Imports database"""
 
     """Connection"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
     
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
@@ -21,6 +22,7 @@ def print_state_n():
 
     cur.close()
     db.close()
+    
 
 if __name__ == "__main__":
     print_state_n()
