2e9a46f1c6e218f915e4c0576521d3efaadc4aa3
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:13:32 -0500

    Whoops

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def print_user_state():
 
     """Connection"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd="Gearsevenbb", db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id;".format(sys.argv[4]))
 
