c7d7f934db559916f1fd5f1f4674a0686b606278
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:12:15 -0500

    Removed binary search

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ def print_user_state():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd="Gearsevenbb", db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\'{}' ORDER BY id;".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id;".format(sys.argv[4]))
 
     """THe data to be printed"""
     row = cur.fetchall()
