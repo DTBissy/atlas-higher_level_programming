6b634365f02a8d3f921526775640f25ce4e10dc4
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:04:43 -0500

    Lets see how this plays out but i added a fourth argv in the query execute for stat names

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Creates a function that coneects to SQL Server"""
+import MySQLdb
+import sys
+
+
+def print_state_n():
+    """Imports database"""
+
+    """Connection"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name LIKE {sys.arg[4]} ORDER BY id;")
+
+    """THe data to be printed"""
+    row = cur.fetchall()
+    for row in row:
+        if str(row[1]).startswith("N"):
+            print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_state_n()
