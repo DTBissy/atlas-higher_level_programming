e4e0e32f14fd8f10940c3df63e9a09953889a47d
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 16:38:45 -0500

    typo

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ import sys
 
 def usr_city_search():
     """The city searcher"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1]
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     
     cur = db.cursor()
