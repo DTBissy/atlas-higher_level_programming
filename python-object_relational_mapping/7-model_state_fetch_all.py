48736b97a83b8ce5b8f16d1f108234c594abda45
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 18:41:39 -0500

    Hmopoh

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Connects to a SQLserver"""
+"""This module creates a connection to a SQL server"""
 
-import sys
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
 
-    username=sys.argv[1]
-    password=sys.argv[2]
-    db=sys.argv[3]
+    """Set arguments"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbase = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/\
-                           {db}')
-    
+    """Create the engine/establish connection"""
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbase}'
+        )
+
+    """Set up all data to use"""
     Base.metadata.create_all(engine)
 
+    """Bind the engine to the session"""
     Session = sessionmaker(bind=engine)
 
+    """Create the session"""
     session = Session()
 
+    """Query the data and list all results, then print"""
     rows = session.query(State).order_by(State.id).all()
     for row in rows:
         print(f"{row.id}: {row.name}")
 
+    """Close the session"""
     Session.close()
