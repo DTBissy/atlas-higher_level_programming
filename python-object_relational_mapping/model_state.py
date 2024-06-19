40a29efd49047f6e371789a560c729ce7571649d
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 17:27:01 -0500

    Forgot the doc string

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Attribute isntantation"""
     __tablename__ = "states"
 
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
