16343d11448a68e0865be68bd038ee99d7f91aff
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:43:20 -0500

    Listss cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Lists all cities"""
+import MySQLdb
+import sys
+
+
+def list_cities():
+    """Works on database"""
+    db = MySQLdb.connect(host="localname", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    
+    cur = db.cursor()
+    query = "SELECT * FROM cities ORDER BY id ASC;"
+    cur.execute(query)
+
+    for row in row:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_cities()
