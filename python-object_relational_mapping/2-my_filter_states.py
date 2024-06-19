a04d1656fc0c0bca2594c82519233228b192eef9
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 15:14:14 -0500

    Line cut

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,8 @@ def print_user_state():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id;".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'\
+                 ORDER BY id;".format(sys.argv[4]))
 
     """THe data to be printed"""
     row = cur.fetchall()
