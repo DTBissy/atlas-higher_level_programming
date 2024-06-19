256fc97742ebc72679b51503095459c785779fd8
Dtbissy <isaactheedwards@gmail.com>
2024-06-19 14:52:46 -0500

    gd

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,7 @@ def print_state_n():
 
     """Connection"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
-    
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
 
@@ -22,7 +21,7 @@ def print_state_n():
 
     cur.close()
     db.close()
-    
+
 
 if __name__ == "__main__":
     print_state_n()
