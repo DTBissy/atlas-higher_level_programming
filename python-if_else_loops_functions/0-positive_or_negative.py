7fff42d69046fa5f5f447ff400db23849064e770
Dtbissy <isaactheedwards@gmail.com>
2024-07-19 14:53:37 -0500

    First Task

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
-if number > 0:
-    print(f"{number} is positive")
+number = random.randint(-500, 500)
+if number < 0:
+    print(f"{number} The number is negative")
 elif number == 0:
-    print(f"{number} is zero")
-else:
-    print(f"{number} is negative")
+    print(f"{number} The number is zero")
+elif number > 0:
+    print(f"{number} The number is positive")
+elif number % 10:
+    print(f"{number} is a funky number")
