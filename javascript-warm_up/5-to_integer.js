498e1c254568a061f7f10a2503fc6b378bb24fdb
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 17:59:35 -0500

    Indention size

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 if (process.argv.length < 2) {
-    console.log("Please provide an argument.");
+  console.log("Please provide an argument.");
 } else {
-    const arg = Number(process.argv[2]);
-    if (isNaN(arg)) {
-        console.log("Not a number");
-    } else {
-        console.log(`My number: ${arg}`);
-    }
+  const arg = Number(process.argv[2]);
+  if (isNaN(arg)) {
+    console.log("Not a number");
+  } else {
+    console.log(`My number: ${arg}`);
+  }
 }
--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -4,7 +4,7 @@ const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 let string = "";
 for (let i = 0; i < array.length; i++) {
-    string += array[i] + "\n";
+  string += array[i] + "\n";
 }
 
 console.log(string.trim());
