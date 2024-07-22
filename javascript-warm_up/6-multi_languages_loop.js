bb1a8f0ed5525c5e560ec644bc143740c35b7ec1
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 18:00:33 -0500

    Fixed

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let string = "";
+let string = '';
 for (let i = 0; i < array.length; i++) {
-  string += array[i] + "\n";
+  string += array[i] + '\n';
 }
 
 console.log(string.trim());
