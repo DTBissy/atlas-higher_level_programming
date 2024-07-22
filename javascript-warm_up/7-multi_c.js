3d18661bef794ccef7f0168e68d5d1bce82c724a
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 18:16:14 -0500

    Refered to the FBI

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log('Missing number of occurrences');
+} else {
+  const count = parseInt(process.argv[2], 10);
+  if (isNaN(count)) {
+    console.log('Missing number of occurences');
+  } else {
+    for (let i = 0; i < count; i++) {
+      console.log('C is fun');
+    }
+  }
+}
