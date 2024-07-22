333ef25240e843a2eebf93e0086698eb2975891b
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 17:57:16 -0500

    Did 5 and 6 and added the rest of the files

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+if (process.argv.length < 2) {
+    console.log("Please provide an argument.");
+} else {
+    const arg = Number(process.argv[2]);
+    if (isNaN(arg)) {
+        console.log("Not a number");
+    } else {
+        console.log(`My number: ${arg}`);
+    }
+}
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let string = "";
+for (let i = 0; i < array.length; i++) {
+    string += array[i] + "\n";
+}
+
+console.log(string.trim());
