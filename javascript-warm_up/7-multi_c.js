6eb9cc2d9e8030dacadedda484fdbeaae199c091
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 18:18:15 -0500

    Removed unessacary check for a count

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,11 +4,7 @@ if (process.argv.length < 3) {
   console.log('Missing number of occurrences');
 } else {
   const count = parseInt(process.argv[2], 10);
-  if (isNaN(count)) {
-    console.log('Missing number of occurences');
-  } else {
-    for (let i = 0; i < count; i++) {
+  for (let i = 0; i < count; i++) {
       console.log('C is fun');
     }
-  }
 }
