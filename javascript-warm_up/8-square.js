c4d307f0b9ee54fb58da76f50a02d4796b8ab583
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 18:55:18 -0500

    Nested loops are smexy loops

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(process.argv[2]);
+
+  if (isNaN(size)) {
+    console.log('Missing size');
+  } else {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
