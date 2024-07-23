037524c8b7f458658c98b8b7ccd1028fa1f69922
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 19:47:45 -0500

    Probably the ugliest thing ive writtten in a while gah damn

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,17 @@ if (process.argv.length < 3) {
   console.log(0);
 } else {
   const numbers = process.argv.slice(2).map(Number);
+  if (numbers.length < 2) {
+    console.log(0);
+  } else {
   function secondbiggest (arr) {
     return arr.sort((a, b) => b - a);
   }
   const sorted = secondbiggest(numbers);
+  if (sorted[1] < 0) {
+    console.log(0);
+  } else {
   console.log(sorted[1]);
+  }
+}
 }
