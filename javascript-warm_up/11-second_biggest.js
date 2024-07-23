a801c732d859d5a685b396395a36f167d662b653
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 19:50:30 -0500

    Wow

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -7,14 +7,14 @@ if (process.argv.length < 3) {
   if (numbers.length < 2) {
     console.log(0);
   } else {
-  function secondbiggest (arr) {
-    return arr.sort((a, b) => b - a);
+    function secondbiggest (arr) {
+      return arr.sort((a, b) => b - a);
+    }
+    const sorted = secondbiggest(numbers);
+    if (sorted[1] < 0) {
+      console.log(0);
+    } else {
+      console.log(sorted[1]);
+    }
   }
-  const sorted = secondbiggest(numbers);
-  if (sorted[1] < 0) {
-    console.log(0);
-  } else {
-  console.log(sorted[1]);
-  }
-}
 }
