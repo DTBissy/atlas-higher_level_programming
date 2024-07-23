8087e5004c4b1159a1684da44d8cfb7efeac407c
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 19:20:10 -0500

    Fixed args and spaces

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 if (process.argv.length < 3) {
-    console.log('Need Number');
+  console.log('NaN');
 } else {
-    const fact = parseInt(process.argv[2]);
+  const fact = parseInt(process.argv[2]);
 
-    function factorial(n) {
-        if (n === 0 || isNaN(n)) {
-            return 1;
-        } else {
-            return n * factorial(n -1);
-        }
+  function factorial(n) {
+    if (n === 0 || isNaN(n)) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
     }
-    console.log(factorial(fact));
+  }
+  console.log(factorial(fact));
 }
