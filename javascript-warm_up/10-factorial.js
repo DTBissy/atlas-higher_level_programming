72ca4bdb2986e007019a985f8a86c842e87cf691
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 19:19:13 -0500

    Thats cool that 333 returns Infinity

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+    console.log('Need Number');
+} else {
+    const fact = parseInt(process.argv[2]);
+
+    function factorial(n) {
+        if (n === 0 || isNaN(n)) {
+            return 1;
+        } else {
+            return n * factorial(n -1);
+        }
+    }
+    console.log(factorial(fact));
+}
