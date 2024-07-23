1fabc6e9e70eec396be7a21e85f88682208820a5
Dtbissy <isaactheedwards@gmail.com>
2024-07-22 20:37:01 -0500

    There

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 if (!process.argv[2]) {
-    console.log(process.argv[2] + ' is ' + process.argv[2]);
-}
-else if (!process.argv[3]){
-    console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
-else {
-    console.log(process.argv[2] + ' is ' + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[2]);
+} else if (!process.argv[3]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
