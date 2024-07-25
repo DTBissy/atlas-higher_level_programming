68a3b4e56fcdf04ec2b5b4dd27f0dcdf619ae92c
Dtbissy <isaactheedwards@gmail.com>
2024-07-25 12:03:20 -0500

    Reada file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const fp = process.argv[2];
+
+fs.readFile(fp, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
