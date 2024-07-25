0197044332ccb45fc434cece6f8b870e7b77f00d
Dtbissy <isaactheedwards@gmail.com>
2024-07-25 13:26:58 -0500

    Google

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const fp = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fp, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
