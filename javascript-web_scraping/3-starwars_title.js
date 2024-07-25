5f2332a3c6080ce847e2446009eee958644444df
Dtbissy <isaactheedwards@gmail.com>
2024-07-25 14:01:24 -0500

    THat was harder than it needed to be

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    } else {
+        const movieData = JSON.parse(body).title;
+        console.log(movieData);
+        console.log(response.statusCode);
+    }
+});
