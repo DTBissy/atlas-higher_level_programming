fda868d25de7e8d012b24c8f4bebd7c0f75a6931
Dtbissy <isaactheedwards@gmail.com>
2024-07-25 14:02:11 -0500

    da

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,11 +6,11 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const movieData = JSON.parse(body).title;
-        console.log(movieData);
-        console.log(response.statusCode);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body).title;
+    console.log(movieData);
+    console.log(response.statusCode);
+  }
 });
