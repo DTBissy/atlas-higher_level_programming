40744a3365f859e976283cc9d2c0be77a56300f6
Dtbissy <isaactheedwards@gmail.com>
2024-07-26 00:47:48 -0500

    hmm

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
+const http = require('http');
 
 const film = process.argv[2];
 
@@ -20,5 +21,16 @@ request(film, function (err, response, body) {
       }
     });
     console.log(count);
-  }
+  }});
+const hostname = '127.0.0.1';
+const port = 5050;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
