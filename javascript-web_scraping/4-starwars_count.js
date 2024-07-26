1b499ca15b3fdb08530dffacced188ce17ebe0ab
Dtbissy <isaactheedwards@gmail.com>
2024-07-26 00:32:15 -0500

    Gah damn rough

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+
+const film = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/people/';
+
+const characterid = '18/';
+
+request(film, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(function (film) {
+      if (film.characters.includes(url + characterid)) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
