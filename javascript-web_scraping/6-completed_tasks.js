8ddc2cd3493670f14ecaca272c65cdbdf6e4b3f6
Dtbissy <isaactheedwards@gmail.com>
2024-07-27 14:12:34 -0500

    H

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(error);
+  } else {
+    const completed = {};
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed) {
+        if (completed[todo.userId]) {
+          completed[todo.userId]++;
+        } else {
+          completed[todo.userId] = 1;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
