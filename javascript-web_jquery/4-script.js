76b3b2ca8c7148741edf9f6ccf33cb637ba602f2
Dtbissy <isaactheedwards@gmail.com>
2024-07-29 15:16:14 -0500

    Crazy how one typo can mess with you

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  $('DIV#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red');
+      $('header').addClass('green');
+    } else {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    }
+  });
+});
